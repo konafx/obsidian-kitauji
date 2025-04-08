ee7e3e1818bd120827b6a42acad04f4aaa3459e5
konafx <22848261+konafx@users.noreply.github.com>
2025-04-08 17:07:59 +0900

    sql本読みだしたのでその演習

---
+++ learning/sql/1-1-1.sql
@@ -0,0 +1,35 @@
+-- 県名を地方名に再分類する
+SELECT CASE pref_name
+	WHEN '徳島' THEN '四国'
+	WHEN '香川' THEN '四国'
+	WHEN '愛媛' THEN '四国'
+	WHEN '高知' THEN '四国'
+	WHEN '福岡' THEN '九州'
+	WHEN '佐賀' THEN '九州'
+	WHEN '長崎' THEN '九州'
+ELSE 'その他' END AS district,
+SUM(population)
+FROM PopTbl
+GROUP BY CASE pref_name
+	WHEN '徳島' THEN '四国'
+	WHEN '香川' THEN '四国'
+	WHEN '愛媛' THEN '四国'
+	WHEN '高知' THEN '四国'
+	WHEN '福岡' THEN '九州'
+	WHEN '佐賀' THEN '九州'
+	WHEN '長崎' THEN '九州'
+ELSE 'その他' END;
+
+-- 地方単位にコードを再分類する その2:Case式を一か所にまとめる
+SELECT CASE pref_name
+	WHEN '徳島' THEN '四国'
+	WHEN '香川' THEN '四国'
+	WHEN '愛媛' THEN '四国'
+	WHEN '高知' THEN '四国'
+	WHEN '福岡' THEN '九州'
+	WHEN '佐賀' THEN '九州'
+	WHEN '長崎' THEN '九州'
+ELSE 'その他' END AS district,
+SUM(population)
+FROM PopTbl
+GROUP BY district;
